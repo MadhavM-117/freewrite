7e0a792078e9231f86670e49b439ee97e048e2a3
Madhav Manoj <madhavmanoj117@gmail.com>
2018-12-20 22:39:28 +0530

    got basic drawing working on app

--- electron-ui/src/react/components/DrawingCanvas/DrawingCanvas.js
+++ electron-ui/src/react/components/DrawingCanvas/DrawingCanvas.js
@@ -1,27 +1,85 @@
 import * as React from 'react';
+
 import './DrawingCanvas.css';
 
 class DrawingCanvas extends React.Component {
+    ctx = null;
+    clickX = [];
+    clickY = [];
+    drawing = false;
+    constructor(props) {
+        super(props);
+        this.draw = this.draw.bind(this);
+        this.endStroke = this.endStroke.bind(this);
+        this.mouseDown = this.mouseDown.bind(this);
+        this.mouseMove = this.mouseMove.bind(this);
+    }
 
     componentDidMount() {
         const canvasRef = this.refs.canvasRef;
-        const ctx = canvasRef.getContext('2d');
-        console.log(ctx);
+        this.ctx = canvasRef.getContext('2d');
+        console.log(this.ctx);
+        this.ctx.strokeStyle = "#df4b26";
+        this.ctx.lineJoin = "round";
+        this.ctx.lineWidth = 5;
     }
 
     render() {
         return (
             <div className="container">
-                <canvas className="canvas" ref="canvasRef" onClick={this.canvasClicked}>
+                <canvas className="canvas" ref="canvasRef" 
+                    onClick={this.canvasClicked} 
+                    onMouseDown={this.mouseDown} 
+                    onMouseMove={this.mouseMove}
+                    onMouseUp={this.endStroke}
+                    onMouseLeave={this.endStroke}
+                    >
                     Hello World!
                 </canvas>
             </div>
         );
     }
 
-    canvasClicked(event) {
-        console.log("Event clicked", event);
+   mouseDown(e) {
+    const {pageX, pageY} = e;
+          
+    // console.log(pageX, pageY);
+   }
+
+   draw(x,y) {
+    if (this.ctx) {
+        if(!this.drawing) {
+            this.drawing = true;
+            this.ctx.beginPath();
+            this.ctx.moveTo(x - 1, y);
+            console.log('started stroke');
+        }
+
+        this.ctx.lineTo(x, y);
+        console.log('drawing', x, y);
     }
+   }
+
+   endStroke(e) {
+    if(this.ctx && this.drawing) {
+        console.log('ended stroke');
+        this.drawing = false;
+        this.ctx.closePath();
+        this.ctx.stroke();
+        // TODO: currently, drawing only ends when the stroke is complete. 
+        // Figure out how to draw as the mouse moves. 
+        // 1. Is it possible to draw points/circles instead of line/strokes?
+        // 2. There is a scaling issue with the drawing. Coordinates don't correspond to actual coordinates drawn in Canvas. 
+        
+    }
+   }
+
+   mouseMove(e) {
+       const {buttons} = e;
+       if (buttons === 1) {
+           this.draw(e.pageX - window.pageXOffset, e.pageY - window.pageYOffset);
+       }
+   }
 }
 
 export default DrawingCanvas;
