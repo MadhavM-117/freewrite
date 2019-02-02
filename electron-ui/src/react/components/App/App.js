5f87bda7eadfb2c9edc1eba199c0457ecf361b67
Madhav Manoj <madhavmanoj117@gmail.com>
2019-02-02 09:52:35 +0530

    WIP: button to screenshot canvas; removed wx-python directory

--- electron-ui/src/react/components/App/App.js
+++ electron-ui/src/react/components/App/App.js
@@ -1,13 +1,15 @@
 import * as React from 'react';
 import './App.css';
 
+import ControlButtons from '../ControlButtons';
 import DrawingCanvas from '../DrawingCanvas';
+
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <DrawingCanvas />
+        <DrawingCanvas /><ControlButtons />
       </div>
     );
   }
